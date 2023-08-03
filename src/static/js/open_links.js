539ca455cc7affc08de7eb08f9c27981a7a6eb97
TechOGR <onelguilarte858@gmail.com>
2023-08-03 13:00:34 -0400

    Updating open_links.js

--- src/static/js/open_links.js
+++ src/static/js/open_links.js
@@ -1,25 +1,15 @@
 const all_img = document.querySelectorAll(".img")
 
-const all_links = {
-    youtube: "https://www.youtube.com/@OnelCrack",
-    facebook: "https://www.facebook.com/profile.php?id=100092376152191",
-    twitter: "https://twitter.com/Onel_Crack?t=NFwmb3M7Gb8dr-B9oUubaw&s=09",
-    instagram: "https://www.instagram.com/onel_crack/",
-    github: "https://github.com/TechOGR"
-}
-
 all_img.forEach((elemento) => {
 
     const function_post = async (name_class) => {
-        let data_send = {
-            name: name_class
-        }
+
         await fetch("/open_link", {
             method: "POST",
             headers: {
                 "Content-Type": 'application/json'
             },
-            body: JSON.stringify(data_send)
+            body: JSON.stringify({ link: name_class })
         })
             .then(response => response.json())
             .then(data => {
@@ -31,8 +21,10 @@ all_img.forEach((elemento) => {
     elemento.addEventListener("click", (event) => {
         const name_link = elemento.className
         const name_to_send = name_link.split(" ")
-        console.log(name_to_send[1])
-        function_post(name_to_send[1])
+        const fileter_name = name_to_send[1]
+
+        function_post(fileter_name)
+
     })
 
 })
