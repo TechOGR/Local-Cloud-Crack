f54115093305d36635b1b734c80cc48191e08efd
TechOGR <onelguilarte858@gmail.com>
2023-08-09 07:47:59 -0400

    Detecting if Android or iOS

--- src/static/js/detect_device.js
+++ src/static/js/detect_device.js
@@ -2,6 +2,6 @@ const user_agent = navigator.userAgent
 
 const footer = document.querySelector("footer")
 
-if (/Mobi|Android/i.test(user_agent)) {
+if (user_agent.match(/Android/i) || user_agent.match(/iPhone/i)) {
     footer.style.cssText = "top: -10px"
 }
